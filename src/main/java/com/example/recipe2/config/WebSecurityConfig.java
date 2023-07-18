5e51c70287c4690f7d3dc96a7f39ba0c08c83857
codegyeon <99954094+codegyeon@users.noreply.github.com>
2023-07-18 21:49:04 +0900

    Update WebSecurityConfig.java

--- src/main/java/com/example/recipe2/config/WebSecurityConfig.java
+++ src/main/java/com/example/recipe2/config/WebSecurityConfig.java
@@ -89,9 +89,9 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000/"));
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
-        config.setAllowedOrigins(List.of("https://dev-recipe-front.vercel.app/"));
+        config.setAllowedOrigins(List.of("https://dev-recipe-front.vercel.app"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
 
